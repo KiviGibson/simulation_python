80c909edc53ddc93bc6129ff38e444b6fbb37527
KiviGibson <77674694+KiviGibson@users.noreply.github.com>
2023-11-22 15:29:13 +0100

    add function to find objects in distance

--- area.py
+++ area.py
@@ -1,3 +1,7 @@
+import math
+from animal import Animal
+
+
 class Area:
     def __init__(self, x: int, y: int) -> None:
         x, y = abs(x), abs(y)
@@ -5,5 +9,14 @@ class Area:
         self.min_x = -x
         self.max_y = y
         self.min_y = -y
-        self.animals = []
-        self.vegetation = []
+        self.objects = []
+
+    def find_in_distance(self, who: Animal) -> list:
+        targets = [i for i in self.objects if i != who]
+        result = []
+        pos = who.pos
+        for target in targets:
+            dist = math.sqrt(pow(pos.x - target.pos.x, 2) + pow(pos.y - target.pos.y, 2))
+            if dist < who.distance:
+                result.append(target)
+        return result
