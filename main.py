56f238109ef3f74ee04408324d7057ccd332ed44
KiviGibson <77674694+KiviGibson@users.noreply.github.com>
2023-11-22 21:52:56 +0100

    set up first enviroment

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from enviroment import Enviroment
+from vector2 import Vector2
 
 
 def start():
-    first = Enviroment()
+    first = Enviroment(0,10, 0, Vector2(20, 20))
 
 
 if __name__ == '__main__':
