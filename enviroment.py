299e9c4f6ee0497e5ba196ebf5be2ce34fd79255
KiviGibson <77674694+KiviGibson@users.noreply.github.com>
2023-11-22 21:54:04 +0100

    add spawning

--- enviroment.py
+++ enviroment.py
@@ -1,10 +1,29 @@
+from vector2 import Vector2
+from arena import Area
+from animal import Animal
+from random import randrange
+
+
 class Enviroment:
-    def __init__(self):
+    def __init__(self, fox_amount: int, rabbit_amount: int, start_grass_amount: int, area_size: Vector2):
         self.is_day = True
+        self.area = Area(int(area_size.x), int(area_size.y))
+        for _ in range(start_grass_amount):
+            ...
+        for _ in range(rabbit_amount):
+            x = randrange(int(-area_size.x), int(area_size.x))
+            y = randrange(int(-area_size.y), int(area_size.y))
+            speed = randrange(5, 10)
+            self.spawn(x, y, speed)
+        for _ in range(fox_amount):
+            ...
+
+    def update(self) -> None:
         ...
 
-    def update(self):
+    def start(self) -> None:
         ...
 
-    def start(self):
-        ...
+    def spawn(self, x: float, y: float, speed: float) -> None:
+        ani = Animal(x, y, self.area, speed=speed)
+        self.area.add_object(ani)
