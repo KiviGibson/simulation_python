ada0d10a4017a86a9920a8497b54331e535181cc
KiviGibson <77674694+KiviGibson@users.noreply.github.com>
2023-11-22 21:55:22 +0100

    change init function to have more nessessery properties

--- animal.py
+++ animal.py
@@ -1,22 +1,22 @@
 from entity import Object
-from area import Area
 
 
 class Animal(Object):
-    def __init__(self, x: float, y: float, distance: float, area: Area) -> None:
-        super().__init__(self, x, y, area)
+    def __init__(self, x: float, y: float, area: object, distance=10.0, collision=2.0, speed=5.0) -> None:
+        super().__init__(x, y, area)
         self.food = []
         self.distance = distance
+        self.collision = collision
+        self.speed = speed
         self.target = None
+        print(x, y, speed)
 
-    def move(self, x: int, y: int, speed: float) -> None:
-        self.pos.x += x * speed
-        self.pos.y += y * speed
+    def move(self, x: int, y: int) -> None:
+        self.pos.x += x * self.speed
+        self.pos.y += y * self.speed
 
     def find_target(self):
         """
         Find next target to hunt / eat
         """
-        objects = self.area.find_in_distance(self)
-        # find best food using numbers
-        ...
+        objects = self.area.find_in_range(self)
